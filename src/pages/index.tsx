b6d00004988e4dfd4e2fee979477cbf91ee29e19
nabeliwo <hiroki.c.watanabe@gmail.com>
2020-02-07 17:03:03 +0900

    :recycle: helmet の位置を変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Helmet from 'react-helmet'
 
 import { App } from '../components/App'
 import { IndexPage } from '../components/pages'
 
 const Index = () => (
   <App>
+    <Helmet>
+      <link href="https://fonts.googleapis.com/css?family=Homemade+Apple&display=swap" rel="stylesheet" />
+    </Helmet>
     <IndexPage />
   </App>
 )
