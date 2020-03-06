96806859ee65d520cbd71fbb77433a3a176352bf
cidermitaina <cidermitaina@gmail.com>
2020-03-06 16:28:56 +0900

    fix: sectionTitleコンポーネントを作成

---
+++ src/components/parts/TechnologyStackSection.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const TechnologyStackSection = () => {
+  return <Section>hoi</Section>
+}
+
+const Section = styled.section``
