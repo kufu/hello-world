61fd4637447f1003125c36af3e79f17e81673920
meganemura <meganemura@users.noreply.github.com>
2020-04-07 09:16:25 +0900

    福利厚生の画像を更新

--- src/components/parts/WelfareSection.tsx
+++ src/components/parts/WelfareSection.tsx
@@ -10,40 +10,40 @@ export const WelfareSection = () => {
       <List>
         <Item>
           <ItemTitle>書籍購入支援</ItemTitle>
-          <img src={'/images/welfare/welfare2.jpg'} width="276" height="193" />
+          <img src={'/images/welfare/welfare1.jpg'} width="276" height="193" />
           <ItemDescription>
             業務で必要な知識を得るための書籍の購入は、稟議不要で申請可能。Slack で Amazon のリンクを送るだけ。
           </ItemDescription>
         </Item>
         <Item>
           <ItemTitle>開発機材購入支援</ItemTitle>
-          <img src={'/images/welfare/welfare1.jpg'} width="276" height="193" />
+          <img src={'/images/welfare/welfare2.jpg'} width="276" height="193" />
           <ItemDescription>
             最大10万円まで PC 周辺機材を購入可能。開発効率を上げるため、好奇心を満たすため、その使い道に制限は無し。
           </ItemDescription>
         </Item>
         <Item>
           <ItemTitle>勉強会参加支援</ItemTitle>
-          <img src={'/images/welfare/welfare2.jpg'} width="276" height="193" />
+          <img src={'/images/welfare/welfare3.jpg'} width="276" height="193" />
           <ItemDescription>
             勉強会への参加費は会社負担。遠方の場合は、交通費および宿泊費も支援。業務として扱うため、休日の場合は振替休日を付与。
           </ItemDescription>
         </Item>
         <Item>
           <ItemTitle>4Kディスプレイ</ItemTitle>
-          <img src={'/images/welfare/welfare1.jpg'} width="276" height="193" />
+          <img src={'/images/welfare/welfare4.jpg'} width="276" height="193" />
           <ItemDescription>
             4K ディスプレイを標準で配布。USB PD 対応で、MacBook に給電しながらデュアルディスプレイでの作業が可能。
           </ItemDescription>
         </Item>
         <Item>
           <ItemTitle>昼寝推奨</ItemTitle>
-          <img src={'/images/welfare/welfare2.jpg'} width="276" height="193" />
+          <img src={'/images/welfare/welfare5.jpg'} width="276" height="193" />
           <ItemDescription>オフィスには昼寝スペース完備。好きな時間に利用可能。もちろん、自席での昼寝も可能。</ItemDescription>
         </Item>
         <Item>
           <ItemTitle>リモートワーク</ItemTitle>
-          <img src={'/images/welfare/welfare1.jpg'} width="276" height="193" />
+          <img src={'/images/welfare/welfare6.jpg'} width="276" height="193" />
           <ItemDescription>
             リモートワークは事前申請のうえ週1回まで可能。好きな時間帯で4時間働けば良い半休制度と併用すると、日中の時間が有効に活用可能。
           </ItemDescription>
