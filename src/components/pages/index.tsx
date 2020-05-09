6802ea7136cee3fed801b589fc4d75c5b65d52cf
nabeliwo <hiroki.c.watanabe@gmail.com>
2020-05-09 22:15:38 +0900

    :sparkles: ファーストビューとなる要素(ヘッダー+キービジュアル)をまとめて window の高さに合わせる

--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -1,9 +1,10 @@
 import React, { useContext } from 'react'
-import styled, { css, keyframes } from 'styled-components'
+import styled, { css } from 'styled-components'
 
 import { mediaQuery } from '../../themes'
 import { CommandPaletteContext } from '../commandPalette'
 
+import { EyeCatch } from '../parts/EyeCatch'
 import { MessageSection } from '../parts/MessageSection'
 import { AboutSection } from '../parts/AboutSection'
 import { TechnologyStackSection } from '../parts/TechnologyStackSection'
@@ -23,29 +24,9 @@ export const IndexPage = () => {
       <EyeCatchEffects />
 
       <Contents>
-        {/* TODO: Headerコンポーネント化する */}
-        <Header>
-          <Left>
-            <Logo>
-              <img src={'/images/logo.svg'} alt="Logo" />
-            </Logo>
-            <Text>Engineer Recruiting</Text>
-          </Left>
-          <Right>
-            <EntryButton href="">ENTRY</EntryButton>
-          </Right>
-        </Header>
+        <EyeCatch />
 
         <Sections>
-          {/* TODO: EyecatchSectionコンポーネント化する */}
-          <EyecatchSection>
-            <PageTitle>
-              <PageTitleImage src="/images/eyecatch/title.svg" alt="歴史に残る模範的なソフトウェアをつくろう" />
-            </PageTitle>
-            <ButtonWrapper>
-              <EntryButtonSp href="">ENTRY</EntryButtonSp>
-            </ButtonWrapper>
-          </EyecatchSection>
           <MessageSection />
           <AboutSection />
           <TechnologyStackSection />
@@ -66,102 +47,6 @@ const Wrapper = styled.div`
 const Contents = styled.div`
   position: relative;
 `
-const Header = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 90px;
-  margin: 0 28px;
-  color: #e8e7e5;
-
-  ${mediaQuery.mediumStyle(css`
-    margin: 0 15px;
-    height: 70px;
-  `)}
-  ${mediaQuery.smallStyle(css`
-    margin: 0 20px;
-    height: 50px;
-
-    img {
-      width: 148px;
-    }
-  `)}
-`
-const Left = styled.div`
-  display: flex;
-`
-const Text = styled.p`
-  font-size: 14px;
-  border-left: 1px solid #797979;
-  margin-left: 18px;
-  padding-left: 18px;
-  display: flex;
-  align-items: center;
-
-  ${mediaQuery.smallStyle(css`
-    font-size: 12px;
-    margin-left: 14px;
-    padding-left: 14px;
-  `)}
-`
-const Logo = styled.div``
-const Right = styled.div`
-  display: flex;
-`
-const EntryButton = styled.a`
-  background-color: #fff;
-  color: #555555;
-  font-size: 16px;
-  font-weight: bold;
-  width: 130px;
-  padding: 12px 0;
-  text-align: center;
-  border-radius: 35px;
-
-  ${mediaQuery.smallStyle(css`
-    display: none;
-  `)}
-`
-const PageTitle = styled.h1`
-  position: relative;
-  line-height: 0;
-  padding-top: 25px;
-
-  ${mediaQuery.mediumStyle(css`
-    padding-top: 167px;
-  `)}
-  ${mediaQuery.smallStyle(css`
-    padding-top: 123px;
-  `)}
-`
-const ButtonWrapper = styled.div`
-  ${mediaQuery.smallStyle(css`
-    text-align: center;
-    margin-top: 145px;
-  `)}
-`
-const EntryButtonSp = styled.a`
-  display: none;
-
-  ${mediaQuery.mediumStyle(css`
-    display: none;
-  `)}
-  ${mediaQuery.smallStyle(css`
-    background-color: #fff;
-    color: #555555;
-    font-size: 20px;
-    font-weight: bold;
-    width: 185px;
-    padding: 16px 0;
-    text-align: center;
-    border-radius: 35px;
-    display: inline-block;
-  `)}
-`
-const PageTitleImage = styled.img`
-  display: block;
-  margin: 0 auto;
-`
 const Sections = styled.div`
   max-width: 1160px;
   margin: 0 auto;
@@ -172,7 +57,4 @@ const Sections = styled.div`
   ${mediaQuery.smallStyle(css`
     padding: 0 20px;
   `)}
-`
-const EyecatchSection = styled.section`
-  height: 100vh;
 `
