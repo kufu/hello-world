1632bcd7de5444cc1c7eaad59dbd5c6353a818d0
cidermitaina <cidermitaina@gmail.com>
2020-05-26 17:38:51 +0900

    fix: adjustment height

--- src/components/parts/EyeCatchEffects.tsx
+++ src/components/parts/EyeCatchEffects.tsx
@@ -44,7 +44,7 @@ const Underlay = styled.div<{ visible: boolean }>`
       top: 0;
       left: 0;
       width: 100%;
-      height: 100%;
+      height: 100vh;
       background-image: url(/images/mv.png);
       background-size: cover;
       background-position: top;
