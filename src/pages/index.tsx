700a33e60e7ad481971adc7d7e329d5ee5ca07d6
nabeliwo <hiroki.c.watanabe@gmail.com>
2020-05-26 22:03:49 +0900

    :lipstick: IE 対応

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,21 @@ import Helmet from 'react-helmet'
 
 import { App } from '../components/App'
 import { IndexPage } from '../components/pages'
+import { IePage } from '../components/pages/ie'
 
-const Index = () => (
-  <App>
-    <Helmet>
-      <link href="https://fonts.googleapis.com/css?family=Homemade+Apple&display=swap" rel="stylesheet" />
-    </Helmet>
-    <IndexPage />
-  </App>
-)
+const Index = () => {
+  const ua = typeof window !== 'undefined' ? window.navigator.userAgent : null
+  const isIe = ua ? ua.indexOf('msie') !== -1 : false
+
+  return (
+    <App>
+      <Helmet>
+        <link href="https://fonts.googleapis.com/css?family=Homemade+Apple&display=swap" rel="stylesheet" />
+      </Helmet>
+
+      {isIe ? <IePage /> : <IndexPage />}
+    </App>
+  )
+}
 
 export default Index
