cbbe67e5499404a7266560f350ab955bfd9bf068
cidermitaina <cidermitaina@gmail.com>
2020-05-26 21:29:46 +0900

    feat: add twitter card tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,10 @@ const Index = () => (
   <App>
     <Helmet>
       <title>Engineer Recruiting | SmartHR</title>
+      <meta
+        name="description"
+        content="2年連続シェアNo.1のクラウド人事労務ソフト「SmartHR」を開発しています。私たちはアナログな世界を、テクノロジーと創意工夫でもっとシンプル、かんたん、便利に変えていきます。歴史に残るソフトウェアを作ろう！"
+      ></meta>
       <meta property="og:title" content="SmartHR エンジニア採用" />
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://hello-world.smarthr.co.jp" />
@@ -18,6 +22,8 @@ const Index = () => (
         content="2年連続シェアNo.1のクラウド人事労務ソフト「SmartHR」を開発しています。私たちはアナログな世界を、テクノロジーと創意工夫でもっとシンプル、かんたん、便利に変えていきます。歴史に残るソフトウェアを作ろう！"
       />
       <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="SmartHR エンジニア採用" />
+      <meta name="twitter:site" content="@SmartHR_jp" />
       <link rel="icon" href="/images/favicon/favicon.ico" />
       <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon-180x180.png" />
       <link href="https://fonts.googleapis.com/css?family=Homemade+Apple&display=swap" rel="stylesheet" />
