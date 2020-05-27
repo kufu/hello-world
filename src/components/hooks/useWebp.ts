6894bcc9aae11076bce0c468a642070917609c66
uknmr <y.kinmori@gmail.com>
2020-05-28 06:18:54 +0900

    :zap: WebP を検知して差し替える

---
+++ src/components/hooks/useWebp.ts
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+export const useWebp = () => {
+  const [enableWebp, setEnableWebp] = useState(false)
+
+  if (typeof Image !== 'undefined') {
+    const img = new Image()
+    img.onload = () => {
+      setEnableWebp(img.width > 0 && img.height > 0)
+    }
+    img.onerror = function() {
+      setEnableWebp(false)
+    }
+    img.src = 'data:image/webp;base64,UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA'
+  }
+
+  return enableWebp
+}
