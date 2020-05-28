5666ae0bbef9b75b34e1131c6539311125cfabab
nabeliwo <hiroki.c.watanabe@gmail.com>
2020-05-28 17:47:19 +0900

    :wrench: GTM を導入

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     // TODO: 正しいタイトルに変更する
-    title: 'SmartHR\'s recruit site',
+    title: "SmartHR's recruit site",
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -9,8 +9,14 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-graphql-codegen',
       options: {
-        fileName: 'types/graphql-types.d.ts'
-      }
-    }
+        fileName: 'types/graphql-types.d.ts',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-google-tagmanager',
+      options: {
+        id: 'GTM-MQXBBK9',
+      },
+    },
   ],
 }
