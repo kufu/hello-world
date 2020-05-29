6c74d6ad4ea8e3f7cda4c4e30d765fe065426ad3
tknzk <info@tknzk.dev>
2020-05-29 18:45:20 +0900

    add new command

--- src/components/commandPalette/commands/index.tsx
+++ src/components/commandPalette/commands/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
 import { AoyagiRun } from './AoyagiRun'
+import { MiyashoGero } from './MiyashoGero'
 
 export const commands = [
   {
     name: 'aoyagi run',
     component: <AoyagiRun />,
+  },
+  {
+    name: 'miyasho gero',
+    component: <MiyashoGero />,
   },
 ]
