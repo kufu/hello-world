b29d52d6b43ed26c90b9eeb6d1f0bd5d542c5813
Shoji Miyata <shojimyt@gmail.com>
2020-05-29 19:40:02 +0900

    MiyashoGeroを削除
    MiyashoGeroのコマンドを削除しました

--- src/components/commandPalette/commands/index.tsx
+++ src/components/commandPalette/commands/index.tsx
@@ -1,15 +1,10 @@
 import React from 'react'
 
 import { AoyagiRun } from './AoyagiRun'
-import { MiyashoGero } from './MiyashoGero'
 
 export const commands = [
   {
     name: 'aoyagi run',
     component: <AoyagiRun />,
-  },
-  {
-    name: 'miyasho gero',
-    component: <MiyashoGero />,
   },
 ]
