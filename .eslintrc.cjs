b7d27a06dd030b945cda624f8bb60a6b87f045e7
taconasu <smi.git.se@gmail.com>
2023-04-05 15:04:10 +0900

    setup eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: [
+    "plugin:astro/all",
+  ],
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {},
+    },
+  ],
+}
