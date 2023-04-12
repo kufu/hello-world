ecddb194c522465f4b00e6f85803ffc804017948
taconasu <smi.git.se@gmail.com>
2023-04-12 14:33:31 +0900

    setup prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,16 @@
 module.exports = {
-  extends: [
-    "plugin:astro/all",
-  ],
+  extends: ['plugin:astro/all', 'plugin:prettier/recommended'],
   overrides: [
     {
-      files: ["*.astro"],
-      parser: "astro-eslint-parser",
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
       parserOptions: {
-        parser: "@typescript-eslint/parser",
-        extraFileExtensions: [".astro"],
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
       },
-      rules: {},
+      rules: {
+        'astro/semi': 'off',
+      },
     },
   ],
 }
