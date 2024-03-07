fb9e7dac7fa8b71d9061cb415b31a272387a03e9
t-mori23 <tmtm3980@gmail.com>
2024-03-07 18:49:28 +0900

    サードパーティライブラリを Web Worker で実行するために Partytown を追加する

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  integrations: [partytown()]
+});
